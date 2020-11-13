2b265705f9f97b5713ca8b937149f53b0f999fef
bvenceslas <bvenceslas@gmail.com>
2020-11-13 20:40:36 +0200

    Connect to db

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.json());
 port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Contaz listens on port ', port));
+app.listen(port, () => console.log('Contaz listens on port: ', port));
